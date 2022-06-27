7e000307db63b71b29ecd3b2ee21e2f809a23541
dmaes30 <david.maes@vdab.be>
2022-06-28 01:32:07 +0200

    Change java version

--- staffplusplus-network-bungee/src/main/java/be/garagepoort/staffplusplusnetwork/network/MessageUtil.java
+++ staffplusplus-network-bungee/src/main/java/be/garagepoort/staffplusplusnetwork/network/MessageUtil.java
@@ -3,6 +3,7 @@ package be.garagepoort.staffplusplusnetwork.network;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static net.md_5.bungee.api.ChatColor.translateAlternateColorCodes;
@@ -10,7 +11,7 @@ import static net.md_5.bungee.api.ChatColor.translateAlternateColorCodes;
 public class MessageUtil {
 
     public static List<BaseComponent> colorizedComponent(String text) {
-        return List.of(TextComponent.fromLegacyText(colorize(text)));
+        return Arrays.asList(TextComponent.fromLegacyText(colorize(text)));
     }
 
     public static String colorize(String text) {
